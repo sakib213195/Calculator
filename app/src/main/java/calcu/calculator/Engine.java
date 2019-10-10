5771e6d63928316d1f85390c3313dd05b82ffb38
Muhammad Sakib Khan <sakib2131@gmail.com>
2019-10-11 00:22:16 +0600

    UI and calculator engine developed

---
+++ app/src/main/java/calcu/calculator/Engine.java
@@ -0,0 +1,25 @@
+package calcu.calculator;
+
+/**
+ * Created by Khan Desktop on 11-Oct-19.
+ */
+
+public class Engine {
+
+    public static double Add (double num1, double num2){
+        return num1 + num2;
+    }
+
+    public static double Sub (double num1, double num2){
+        return num1 - num2;
+    }
+
+    public static double Mul (double num1, double num2){
+        return num1 * num2;
+    }
+
+    public static double Div (double num1, double num2){
+        return num1 / num2;
+    }
+
+}
